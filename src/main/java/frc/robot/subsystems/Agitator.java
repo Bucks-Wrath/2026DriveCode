63839fd3002c2e217d37399b01af10851a9ee086
6329 Code Team <john.boynton@rsu25.org>
2026-01-20 15:05:24 -0500

    add agitator

---
+++ src/main/java/frc/robot/subsystems/Agitator.java
@@ -0,0 +1,84 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.DeviceIds;
+
+public class Intake extends SubsystemBase {
+
+	private TalonFX IntakeKraken = new TalonFX(DeviceIds.Intake.LeadMotorId);
+    private TalonFXConfiguration IntakeFXConfig = new TalonFXConfiguration();
+    private TalonFX IntakeKrakenFollower = new TalonFX(DeviceIds.Intake.FollowerMotorId);
+
+
+	public Intake() {
+        /** Shooter Motor Configuration */
+        /* Motor Inverts and Neutral Mode */
+		IntakeFXConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        IntakeFXConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+        // Set Followers
+		IntakeKrakenFollower.setControl(new Follower(IntakeKraken.getDeviceID(), null));
+
+        /* Current Limiting */
+        //IntakeFXConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+        //IntakeFXConfig.CurrentLimits.SupplyCurrentLimit = 20;
+        //IntakeFXConfig.CurrentLimits.SupplyCurrentThreshold = 30;
+        //IntakeFXConfig.CurrentLimits.SupplyTimeThreshold = 0.01;
+
+        IntakeFXConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+        IntakeFXConfig.CurrentLimits.StatorCurrentLimit = 25;
+
+        /* PID Config */
+        IntakeFXConfig.Slot0.kP = 0.2;
+        IntakeFXConfig.Slot0.kI = 0;
+        IntakeFXConfig.Slot0.kD = 0;
+
+        /* Open and Closed Loop Ramping */
+        IntakeFXConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 0.1;
+        IntakeFXConfig.OpenLoopRamps.VoltageOpenLoopRampPeriod = 0.1;
+
+        IntakeFXConfig.ClosedLoopRamps.DutyCycleClosedLoopRampPeriod = 0.1;
+        IntakeFXConfig.ClosedLoopRamps.VoltageClosedLoopRampPeriod = 0.1;
+
+        // Config Motor
+        IntakeKraken.getConfigurator().apply(IntakeFXConfig);
+        IntakeKraken.getConfigurator().setPosition(0.0);
+        IntakeKrakenFollower.getConfigurator().setPosition(0.0);
+	}
+
+	public void setSpeed(double speed) {
+        this.IntakeKraken.set(speed);
+	}
+
+	public double getCurrentDrawLeader() {
+		return this.IntakeKraken.getSupplyCurrent().getValueAsDouble();
+	}
+
+    public double getCurrentDrawFollower() {
+		return this.IntakeKrakenFollower.getSupplyCurrent().getValueAsDouble();
+	}
+
+	public void resetShooterEncoder() {
+        try {
+			IntakeKraken.getConfigurator().setPosition(0.0);
+            IntakeKrakenFollower.getConfigurator().setPosition(0.0);
+        }
+        catch (Exception e) {
+            DriverStation.reportError("Shooter.resetShooterEncoders exception.  You're Screwed! : " + e.toString(), false);
+        }
+	}
+
+	public void updateDashboard() {
+		SmartDashboard.putNumber("Intake Current", this.getCurrentDrawLeader());
+        SmartDashboard.putNumber("Intake Follower Current", this.getCurrentDrawFollower());
+
+	}
+}

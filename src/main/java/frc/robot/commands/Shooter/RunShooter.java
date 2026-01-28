efad31773f45b3fcf43503e5d2e336add0920521
6329 Code Team <john.boynton@rsu25.org>
2026-01-28 16:04:25 -0500

    run/stop shooter

---
+++ src/main/java/frc/robot/commands/Shooter/RunShooter.java
@@ -0,0 +1,38 @@
+package frc.robot.commands.Shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
+
+public class RunShooter extends Command {
+    
+    public RunShooter() {
+        addRequirements(RobotContainer.shooter);
+    }
+	// Called just before this Command runs the first time
+	public void initialize() {
+
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	public void execute() {
+        RobotContainer.shooter.setSpeed(1.0);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	public boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		RobotContainer.shooter.setSpeed(0);
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
+}
+
+
